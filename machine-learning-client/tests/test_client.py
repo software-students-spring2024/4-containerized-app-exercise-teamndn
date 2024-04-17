be624a2cc23a029cdf3ffd1c1d263fde8191492d
Daniel Pang <dp3228@nyu.edu>
2024-04-16 22:28:08 -0400

    finished tests

--- machine-learning-client/tests/test_client.py
+++ machine-learning-client/tests/test_client.py
@@ -1,10 +1,17 @@
-import MLClient
 import cv2
+import pymongo
+def faceCounter(img):
+        faceClassifier = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+        image = cv2.imread(img)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        faces = faceClassifier.detectMultiScale(image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+        return len(faces)
+
 class Tests:
     def sanity_check():
         assert True == True, "True does not equal True!"
 
     def testFaceCounter(self):
-        len = facecounter(test.jpg)
+        len = faceCounter("tests/test.jpg")
         assert isinstance(len, int)
         
